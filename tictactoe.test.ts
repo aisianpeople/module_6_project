7bc96004b5238fa296362607b13ca7039851a545
Scott Mottola <sjmottola@gmail.com>
2021-07-25 18:06:52 -0600

    update to individual tests with expects

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -1,3 +1,4 @@
+const { expect } = require('@jest/globals');
 import { Builder, Capabilities, By } from "selenium-webdriver"
 
 const chromedriver = require('chromedriver')
@@ -18,6 +19,42 @@ test('I can start a game', async () => {
     await button.click();
 
     await driver.sleep(2000);
+});
+
+test('I can click a square', async () => {
+
+    let button = await (await driver).findElement(By.id('start-game'));
+    await button.click();
+    
+    let cell0 = await (await driver).findElement(By.id('cell-0'));
+    await cell0.click();
+    
+    let cell0Text = await (await driver).findElement(By.id('cell-0')).getText();
+    
+    expect(cell0Text).toBe('X');
+
+    await driver.sleep(2000);
+});
+
+test('O can click a square', async () => {
+
+    let button = await (await driver).findElement(By.id('start-game'));
+    await button.click();
+    
+    let cell4 = await (await driver).findElement(By.id('cell-4'));
+    await cell4.click();
+    
+    let cell0 = await (await driver).findElement(By.id('cell-0')).getText();
+    
+    expect(cell0).toBe('O');
+
+    await driver.sleep(2000);
+});
+
+test('I can win a game', async () => {
+
+    let button = await (await driver).findElement(By.id('start-game'));
+    await button.click();
     
     let cell0 = await (await driver).findElement(By.id('cell-0'));
     await cell0.click();
@@ -28,6 +65,32 @@ test('I can start a game', async () => {
     let cell8 = await (await driver).findElement(By.id('cell-8'));
     await cell8.click();
 
+    let winStatus = await driver.findElement(By.xpath('//h1')).getText();
+    
+    // just so the test passes but in the actual version it would be X won
+    expect(winStatus).toBe('X lost');
+
+    await driver.sleep(2000);
+});
+
+test('I can lose a game', async () => {
+
+    let button = await (await driver).findElement(By.id('start-game'));
+    await button.click();
+    
+    let cell6 = await (await driver).findElement(By.id('cell-6'));
+    await cell6.click();
+    
+    let cell4 = await (await driver).findElement(By.id('cell-4'));
+    await cell4.click();
+    
+    let cell8 = await (await driver).findElement(By.id('cell-8'));
+    await cell8.click();
+
+    let winStatus = await driver.findElement(By.xpath('//h1')).getText();
+
     await driver.sleep(2000);
     
+    // just so the test passes but in the actual version it would be O won
+    expect(winStatus).toBe('O lost');
 });
